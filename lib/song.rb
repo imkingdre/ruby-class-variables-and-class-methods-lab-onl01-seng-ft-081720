d7e3c9b129bd227d5ff4daad40a2fa8c2c0f95be
Andre King <andreking1031@gmail.com>
2020-08-26 23:10:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,5 @@
 class Song
-  def name=(name)
-    @name = name
-  end
-  def artist=(artist)
-    @artist = artist
-  end
-  def genre=(genre)
-    @genre = genre
-  end
+  attr_accessor :name, :artist, :genre
+  
   
 end
