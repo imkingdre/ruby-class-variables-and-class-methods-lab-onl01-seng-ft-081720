8b057791cbb665ad464a51f135632740c647a70b
Andre King <andreking1031@gmail.com>
2020-08-26 23:27:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,16 +4,16 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  def initialize(name, artist, genre)
-    @name = name
+  def initialize(song_name, artist, genre)
+    @name = song_name
     @artist = artist
     @genre = genre
     @@genres << @genre
     @@artists << @artist
-    @@song_count += 1
+    @@count += 1
   end
   def self.count
-    @@song_count
+    @@count
   end
   def self.genres
     @@genres.uniq!
